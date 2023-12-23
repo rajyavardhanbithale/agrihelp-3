bf9e96de32d32947943b8f66188ccb25c0667042
rajyavardhanbithale <bithale03@gmail.com>
2023-12-23 21:03:41 +0530

    v3.r71

--- client/src/app/components/EndpointError.jsx
+++ client/src/app/components/EndpointError.jsx
@@ -22,7 +22,7 @@ export default function EndpointError() {
                             OR
                         </p>
                         <p className="mt-4 text-xl text-gray-500">
-                            If you have urgent matters or inquiries, please don't hesitate to reach out to us via email:
+                            If you have urgent matters or inquiries, please don&apos;t hesitate to reach out to us via email:
                             <div className="flex flex-col gap-2 mt-2">
                                 <a href="mailto:rajyavardhan@aol.com">rajyavardhan@aol.com</a>
                                 <a href="mailto:bithale01@gmail.com">bithale01@gmail.com</a>
